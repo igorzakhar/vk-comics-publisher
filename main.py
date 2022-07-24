47e4f7a883e78d950ffeb676320a19cb3260455a
Igor Zakharov <igorzakhar@gmail.com>
2022-07-24 20:37:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,175 @@
+import argparse
+import logging
+import os
+import random
+
+from pathlib import Path
+
+from urllib.parse import urlparse
+
+import requests
+
+from dotenv import load_dotenv
+
+
+def download_random_comics(total_number):
+    random.seed()
+    num = random.randint(1, total_number)
+    api_url = f'https://xkcd.com/{num}/info.0.json'
+
+    response = requests.get(api_url)
+    response.raise_for_status()
+
+    comics_metadata = response.json()
+    img_link = comics_metadata.get("img")
+
+    alt = comics_metadata.get("alt")
+
+    response = requests.get(img_link)
+    response.raise_for_status()
+
+    url_path = urlparse(img_link).path
+    comics_filename = Path(url_path).name
+    with open(comics_filename, 'wb') as fp:
+        fp.write(response.content)
+
+    return comics_filename, alt
+
+
+def _upload_comics(base_url, token, api_version, filename):
+    vk_method = 'photos.getWallUploadServer'
+    params = {'access_token': token, 'v': api_version}
+    endpoint_url = f'{base_url}{vk_method}'
+    response = requests.get(endpoint_url, params=params)
+    response.raise_for_status()
+
+    upload_url = response.json()['response']['upload_url']
+    with open(filename, 'rb') as fp:
+        files = {
+            'photo': fp
+        }
+        upload_response = requests.post(upload_url, files=files)
+        upload_response.raise_for_status()
+
+    upload_metadata = upload_response.json()
+    server = upload_metadata.get('server')
+    photo = upload_metadata.get('photo')
+    photo_hash = upload_metadata.get('hash')
+
+    return server, photo, photo_hash
+
+
+def _save_comics(base_url, token, api_ver, server, photo, photo_hash):
+    vk_method = 'photos.saveWallPhoto'
+    params = {
+        'access_token': token,
+        'v': api_ver,
+        'server': server,
+        'photo': photo,
+        'hash': photo_hash,
+    }
+    endpoint_url = f'{base_url}{vk_method}'
+    save_photo_response = requests.post(endpoint_url, params=params)
+    save_photo_response.raise_for_status()
+
+    saved_photo_metadata = save_photo_response.json()
+
+    owner_id = saved_photo_metadata['response'][0]['owner_id']
+    photo_id = saved_photo_metadata['response'][0]['id']
+
+    return owner_id, photo_id
+
+
+def _post_comics(base_url, token, api_ver, group_id, owner_id, photo_id, alt):
+    vk_method = 'wall.post'
+    params = {
+        'access_token': token,
+        'v': api_ver,
+        'owner_id': f'-{group_id}',
+        'from_group': 1,
+        'attachments': f'photo{owner_id}_{photo_id}',
+        'message': alt,
+    }
+    endpoint_url = f'{base_url}{vk_method}'
+    response = requests.get(endpoint_url, params=params)
+    response.raise_for_status()
+    return response.json()
+
+
+def post_comics_on_wall(group_id, token, api_ver, filename, alt):
+    api_base_url = 'https://api.vk.com/method/'
+
+    server, photo, photo_hash = _upload_comics(
+        api_base_url,
+        token,
+        api_ver,
+        filename
+    )
+
+    owner_id, photo_id = _save_comics(
+        api_base_url,
+        token,
+        api_ver,
+        server,
+        photo,
+        photo_hash
+    )
+
+    post_metadata = _post_comics(
+        api_base_url,
+        token,
+        api_ver,
+        group_id,
+        owner_id,
+        photo_id,
+        alt
+    )
+
+    logging.debug(post_metadata)
+
+
+def process_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', '--debug', action='store_true')
+    return parser.parse_args()
+
+
+def main():
+    load_dotenv()
+
+    args = process_args()
+
+    logging.getLogger('urllib3').setLevel(logging.WARNING)
+    logging_level = logging.INFO
+    if args.debug:
+        logging_level = logging.DEBUG
+
+    logging.basicConfig(
+        level=logging_level,
+        format='%(funcName)s() \u2192  %(message)s'
+    )
+
+    vk_group_id = os.getenv('VK_GROUP_ID')
+    vk_token = os.getenv('VK_ACCESS_TOKEN')
+    vk_api_version = os.getenv('VK_VERSION_API')
+
+    xkcd_response = requests.get('https://xkcd.com/info.0.json')
+    xkcd_response.raise_for_status()
+    comics_total_number = xkcd_response.json().get('num')
+
+    comics_filename, alt = download_random_comics(comics_total_number)
+
+    post_comics_on_wall(
+        vk_group_id,
+        vk_token,
+        vk_api_version,
+        comics_filename,
+        alt
+    )
+
+    file_to_remove = Path(comics_filename)
+    file_to_remove.unlink()
+
+
+if __name__ == '__main__':
+    main()
